627e1a0f7c06a7a52a22e55a6f055141caf303b6
achoma <139071702+achoma@users.noreply.github.com>
2024-03-01 09:34:12 +0100

    up

--- js/slider.js
+++ js/slider.js
@@ -1,33 +1,24 @@
-var splide = new Splide('#Our-products', {
-    perPage: 4,
-    focus: 0,
-    arrows: false,
-    wheel: true,
-    breakpoints: {
-      1199: {
-        perPage: 2.5,
-      },
-      767: {
-        perPage: 1,
-      },
-    },
+(() => {
+  const modalButtons = document.querySelectorAll('[data-modal-open]');
+  const closeButtons = document.querySelectorAll('[data-modal-close]');
+  
+  modalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modalId = button.getAttribute('data-modal-open');
+      const modal = document.querySelector(`[data-modal="${modalId}"]`);
+      if (modal) {
+        modal.classList.remove('is-hidden');
+      }
+    });
   });
-  
-  splide.mount();
-  
-  var splide = new Splide('#slider2', {
-    perPage: 3,
-    focus: 0,
-    arrows: false,
-    wheel: true,
-    breakpoints: {
-      1199: {
-        perPage: 2,
-      },
-      767: {
-        perPage: 1,
-      },
-    },
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modalId = button.getAttribute('data-modal-close');
+      const modal = document.querySelector(`[data-modal="${modalId}"]`);
+      if (modal) {
+        modal.classList.add('is-hidden');
+      }
+    });
   });
-  
-  splide.mount();
+})();
